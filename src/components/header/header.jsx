1b004cd6eea8275f5f51137d45095ad979273d67
jfiora <sillonpeludo@gmail.com>
2021-12-09 13:43:05 -0300

    add styles for button

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import sunIcon from "../../images/icon-sun.svg";
 import moonIcon from "../../images/icon-moon.svg";
+import "./header.css";
 
 const Header = () => {
   const [show, setShow] = useState(false);
